30ad443b60fc1afb444095babb6bab33dcdad65c
alpsakaci <erenalpsakaci@gmail.com>
2024-09-22 10:18:25 +0300

    add keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,6 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
 vim.api.nvim_del_keymap("n", "s")
 vim.api.nvim_del_keymap("v", "s")
+-- Increase window width
+vim.api.nvim_set_keymap("n", "<S-Right>", ":vertical resize +10<CR>", { noremap = true, silent = true })
+-- Decrease window width
+vim.api.nvim_set_keymap("n", "<S-Left>", ":vertical resize -10<CR>", { noremap = true, silent = true })
