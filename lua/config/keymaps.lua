8f82e006f282fe47e3228d78be1fdd35328bb816
alpsakaci <erenalpsakaci@gmail.com>
2024-10-26 10:36:32 +0300

    add window resize keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,7 @@ vim.api.nvim_del_keymap("v", "s")
 vim.api.nvim_set_keymap("n", "<S-Right>", ":vertical resize +10<CR>", { noremap = true, silent = true })
 -- Decrease window width
 vim.api.nvim_set_keymap("n", "<S-Left>", ":vertical resize -10<CR>", { noremap = true, silent = true })
+-- Increase window height
+vim.api.nvim_set_keymap("n", "<S-Up>", ":horizontal resize +10<CR>", { noremap = true, silent = true })
+-- Decrease window height
+vim.api.nvim_set_keymap("n", "<S-Down>", ":horizontal resize -10<CR>", { noremap = true, silent = true })
